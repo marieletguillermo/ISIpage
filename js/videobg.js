f7bd5c9a5925f2ee4a4f3e81a0e2458ca1d2c168
Athena <athena_pascua@gmail.com>
2020-06-10 13:00:18 +0800

    Update html files

--- js/videobg.js
+++ js/videobg.js
@@ -60,10 +60,42 @@ this.player.seekTo(0)}}}})(void 0);
                 "loop": true,           //Loop when it reaches the end
                 "autoplay": true,       //Autoplay at start
                 "muted": true,          //Muted at start
+                "mp4":"upload/video.mp4" ,     //Path to video mp4 format
+                "ogg":"upload/video.ogg" ,     //Path to video ogg format
+                "webm":"upload/video.webm" ,     //Path to video webm format
+                "video_ratio": 1.7778,              // width/height -> If none provided sizing of the video is set to adjust
+                "fallback_image": "images/dummy.png",   //Fallback image path
+                "priority": "html5"             //Priority for html5 (if set to flash and tested locally will give a flash security error)
+            });
+        });
+
+        jQuery(document).ready(function($) {
+            var Video_back = new video_background($("#container"), { 
+                "position": "absolute", //Follow page scroll
+                "z-index": "-1",        //Behind everything
+                "loop": true,           //Loop when it reaches the end
+                "autoplay": true,       //Autoplay at start
+                "muted": true,          //Muted at start
                 "mp4":"upload/preview.mp4" ,     //Path to video mp4 format
                 "ogg":"upload/preview.ogg" ,     //Path to video ogg format
                 "webm":"upload/preview.webm" ,     //Path to video webm format
-                "video_ratio": 1.7778,              // width/height -> If none provided sizing of the video is set to adjust
+                "video_ratio": 0.7,              // width/height -> If none provided sizing of the video is set to adjust
+                "fallback_image": "images/dummy.png",   //Fallback image path
+                "priority": "html5"             //Priority for html5 (if set to flash and tested locally will give a flash security error)
+            });
+        });
+
+        jQuery(document).ready(function($) {
+            var Video_back = new video_background($("#banner"), { 
+                "position": "absolute", //Follow page scroll
+                "z-index": "-1",        //Behind everything
+                "loop": true,           //Loop when it reaches the end
+                "autoplay": true,       //Autoplay at start
+                "muted": true,          //Muted at start
+                "mp4":"upload/banner.mp4" ,     //Path to video mp4 format
+                "ogg":"upload/preview.ogg" ,     //Path to video ogg format
+                "webm":"upload/preview.webm" ,     //Path to video webm format
+                "video_ratio": 0.7,              // width/height -> If none provided sizing of the video is set to adjust
                 "fallback_image": "images/dummy.png",   //Fallback image path
                 "priority": "html5"             //Priority for html5 (if set to flash and tested locally will give a flash security error)
             });
